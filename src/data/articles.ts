1ed24f54d67c6a22b61154dbc67809e2ce4b45e5
Arpad Muranyi <pesz@pesz.hu>
2024-02-03 07:16:36 +0100

    European bitcoiners, logos resize 200x64, styling

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -4,14 +4,14 @@ export const articles = [
     {
         name: "Mi az a Bitcoin?",
         lang: "hu",
-        text: "Mi az a Bitcoin, és hogyan működik? Kezdő szint",
+        text: "Mi az a Bitcoin, és hogyan működik?",
         link: "https://europeanbitcoiners.com/mi-az-a-bitcoin/",
         img: "/src/assets/article/mi-az-a-bitcoin.png"
     },
     {
         name: "Hogy működik a Bitcoin?",
         lang: "hu",
-        text: "A bitcoin decentralizált digitális valuta. De hogy működik? Hogy jönnek létre a tranzakciók, és miként tartják fenn decentralizált, bizalom nélküli módon?",
+        text: "Hogy jönnek létre a tranzakciók, és miként tartják fenn decentralizált, bizalom nélküli módon?",
         link: "https://europeanbitcoiners.com/hogy-mukodik-a-bitcoin/",
         img: "/src/assets/article/hogy-mukodik-a-bitcoin.png"
     },
