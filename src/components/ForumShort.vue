ea249dbd4b754fa6922a3b759233730cce691271
Arpad Muranyi <pesz@pesz.hu>
2024-02-08 22:24:49 +0100

    Telegram group button

--- src/components/ForumShort.vue
+++ src/components/ForumShort.vue
@@ -3,18 +3,23 @@
 
 <template>
     <div class="card py-5 px-5 my-3 mx-4">
-        <p class="card-text">
-            Közösségünk fő találkozási pontja a
-            <b>huszonegy telegram csoport:</b> <a href="https://t.me/huszonegy" target="_blank">t.me/huszonegy</a>
-        </p>
         <p class="card-text center">
+            Közösségünk fő találkozási pontja a
+            <b>huszonegy telegram csoport.</b>
+            <br />
+            <br />
+        </p>
+<!--        <p class="card-text center">
             <a href="https://t.me/huszonegy" target="_blank">
                 <img src="/logo/he-logo-tgram-1-200x64.png" width="150" height="48" class="my-3"/>
             </a>
         </p>
-        <p class="card-text my-2">
-            Csatlakozz Te is! Nézz körül, kérdezz, javasolj!
-        </p>
+-->
+        <center>
+              <a href="https://t.me/huszonegy" target="_blank">
+                <button type="button" class="btn btn-custom">Csatlakozz a csoporthoz</button>
+              </a>
+        </center>
     </div>
 </template>
 
