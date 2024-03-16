4bc9caa825a64a6017725481b123cff3152f0442
Arpad Muranyi <pesz@pesz.hu>
2024-03-16 06:32:25 +0100

    Podcast page update

--- src/data/podcasts.ts
+++ src/data/podcasts.ts
@@ -6,6 +6,7 @@ export const podcasts = [
         img: "/pics/mi-az-a-bitcoin-s01e04.jpg",
         date: "2024.03.14.",
         members: ["Anti", "Kata", "Feri"],
+        topic: "öngondoskodás, megtakarítás, bitcoin zsebpénz, oktatás, egészségügy, háború, kizsákmányolás, vidéki meetup-ok, Bitcoin konferencia",
         yt: "https://www.youtube.com/watch?v=WY419JvU1Zs",
         fountain: "https://fountain.fm/episode/odzEIohsIkEPxXEyZgYn"
     },
@@ -14,6 +15,7 @@ export const podcasts = [
         img: "/pics/mi-az-a-bitcoin-s01e03.jpg",
         date: "2024.03.08.",
         members: ["Anti", "Roli", "Árpi"],
+        topic: "Bitcoin Atlantis konferencia, Madeira, 1 BTC = 1 BTC, minden osztva 21 millióval, időlánc/timechain, adósság spirál",
         yt: "https://www.youtube.com/watch?v=vliH7iNDbVk",
         fountain: "https://fountain.fm/episode/0lMbuTipmKkGflGnWtyn"
     },
@@ -22,6 +24,7 @@ export const podcasts = [
         img: "/pics/mi-az-a-bitcoin-s01e02.jpg",
         date: "2024.02.29.",
         members: ["Anti", "Árpi"],
+        topic: "a bitcoin kibocsátása, bitcoin bányászat, főkönyv, csomópontok, blokk jutalom, felezés, szűkösség és garantáltan csökkenő infláció",
         yt: "https://www.youtube.com/watch?v=wMfGvxNRkEM",
         fountain: "https://fountain.fm/episode/CLQiTVsh1Wjb0qnoaryN"
     },
@@ -30,6 +33,7 @@ export const podcasts = [
         img: "/pics/mi-az-a-bitcoin-s01e01.jpg",
         date: "2024.02.23.",
         members: ["Anti", "Optout", "Árpi"],
+        topic: "bevezető",
         yt: "https://www.youtube.com/watch?v=JEu3CcID0dU",
         fountain: "https://fountain.fm/episode/h4aHUSZkaYeFIMgqRAoA"
     }
