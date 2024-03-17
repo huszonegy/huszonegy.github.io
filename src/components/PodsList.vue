82c4c05eed17c70a23c60c40084ae50aba791c22
Arpad Muranyi <pesz@pesz.hu>
2024-03-17 18:12:01 +0100

    Bitcoin kebab on podcast page

--- src/components/PodsList.vue
+++ src/components/PodsList.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { podcasts } from '../data/podcasts'
+import { kebabs } from '../data/kebabs'
 import { link_simplified } from '../utils'
 </script>
 
 <template>
-    <h1><a href='https://www.youtube.com/@huszonegy-bitcoin-podcast' target='_blank'>Mi az a Bitcoin?</a> podcast</h1>
-    <h2 style="margin-top: 20px;">
+    <h1>Bitcoin podcast magyarul</h1>
+    <h2><a href='https://www.youtube.com/@huszonegy-bitcoin-podcast' target='_blank'>Mi az a Bitcoin?</a> podcast</h2>
+    <h3 style="margin-top: 20px;">
         Heti rendszerességgel jelentkezünk új tartalommal.
-    </h2>
-    <h3>
-        Kizárólag Bitcoinnal foglalkozunk, és fő célunk a magyar nyelvű Bitcoin témájú tájékoztatás mennyiségének, minőségének, közérthetőségének és valóságtartalmának intenzív javítása.
     </h3>
+    <p>
+        Kizárólag Bitcoinnal foglalkozunk, és fő célunk a magyar nyelvű Bitcoin témájú tájékoztatás mennyiségének, minőségének, közérthetőségének és valóságtartalmának intenzív javítása.
+    </p>
     <div name="links-list" class="container-flex py-3 podcast">
         <div class="row row-cols-2">
             <div v-for="pod in podcasts" class="card mx-2 my-2" style="width: 20rem;">
@@ -42,6 +44,38 @@ import { link_simplified } from '../utils'
             </div>
         </div>
     </div>
+
+    <h2>Bitcoin Kebab podcast</h2>
+    <p>
+        Válogatás a BitcoinBázis podcastjának adásaiból.
+    </p>
+    <div name="links-list" class="container-flex py-3 podcast">
+        <div class="row row-cols-2">
+            <div v-for="kebab in kebabs" class="card mx-2 my-2" style="width: 20rem;">
+                <div class="card-body">
+                    <h4>
+                        <img :src="kebab.img" alt="Bitcoin Kebab" />
+                    </h4>
+                    <h5 class="card-title">
+                        {{ kebab.name }}
+                    </h5>
+                    <p class="small my-3 grey">
+                        {{ kebab.date }}<span v-for="member in kebab.members">&nbsp;|&nbsp;{{ member }}</span> 
+                    </p>
+                    <p class="small my-3">
+                        youtube: 
+                        <a :href="kebab.yt" target="_blank" class="link">
+                            video
+                        </a>
+                    </p>
+                    <p class="small my-3">
+                        {{ kebab.topic }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style scoped>
---
+++ src/data/kebabs.ts
@@ -0,0 +1,69 @@
+// Data for Kebabs
+
+export const kebabs = [
+    {
+        name: "Lightning Network",
+        img: "/pics/bitcoin-kebab-240219.png",
+        date: "2024.02.19.",
+        members: ["Kata", "Optout", "Openoms"],
+        topic: "Mi az a Lightning Network? Hogyan működik? Milyen díjai vannak? Meddig skálázható? Hogyan tudunk bitcoint ajándékozni anélkül, hogy tudnánk az ajándékozni kívánt fél tárcacímét?",
+        yt: "https://www.youtube.com/watch?v=QqwJN9unBIQ",
+    },
+    {
+        name: "A megismételhetetlen Bitcoin",
+        img: "/pics/bitcoin-kebab-231030.png",
+        date: "2023.10.30.",
+        members: ["Kata", "Knut Svanholm"],
+        topic: "Matrózból bitcoiner, a BTC Infinity Day eredete, a praxiológia és a Bitcoin kapcsolata, madeirai élmények, pár jótanácsal kezdők számára.",
+        yt: "https://www.youtube.com/watch?v=9TlEA2FlIUA",
+    },
+    {
+        name: "A digitális anonimitás fontossága",
+        img: "/pics/bitcoin-kebab-231009.png",
+        date: "2023.10.09.",
+        members: ["Kata", "Optout"],
+        topic: "Miért fontos anonimnak maradni egyes helyzetekben, névtelen bizalomépítés, Bitcoin fejlesztői vélemények a Bitcoinnal kapcsolatban, könyvajánló.",
+        yt: "https://www.youtube.com/watch?v=VCd84pEeO_Y",
+    },
+    {
+        name: "A világ legnagyobb pilótajátéka",
+        img: "/pics/bitcoin-kebab-230904.png",
+        date: "2023.09.04.",
+        members: ["Kata", "Alex Gladstein"],
+        topic: "Hogyan formálják az IMF és a Világbank politikái a világ fejlődését, és mindez hogyan kapcsolódik az emberi jogokhoz, a technológiához és a bitcoinhoz.",
+        yt: "https://www.youtube.com/watch?v=rzOMbfTRjwg",
+    },
+    {
+        name: "Kriptokalauz bitcoin stopposoknak",
+        img: "/pics/bitcoin-kebab-230821.png",
+        date: "2023.08.21.",
+        members: ["Kata", "Feri"],
+        topic: "Egy vezető, aki közvetlen környezetében önként vállalt feladatának érzi a bitcoin megértetését és használatának elterjedését. DYOR vagyis a “végezd el a saját kutatásod” folyamata.",
+        yt: "https://www.youtube.com/watch?v=PTqlAq80hOY",
+    },
+    {
+        name: "Lightning Network Proof-of-Stake",
+        img: "/pics/bitcoin-kebab-230515.png",
+        date: "2023.05.15.",
+        members: ["Kata", "Sziller"],
+        topic: "Bitcoin fejlesztőként az orákulum problémáról, a kvantumszámítógépekről, az Ethereumról, a Lightning Network-ről és a node-ok működéséről.",
+        yt: "https://www.youtube.com/watch?v=ovRZQ-Wzt88",
+    },
+    {
+        name: "A blokklánc rettenetes adatbázis!",
+        img: "/pics/bitcoin-kebab-230508.png",
+        date: "2023.05.08.",
+        members: ["Kata", "Sziller"],
+        topic: "Bitcoin, Lightning Network, tranzakciós költségek, skálázhatóság, blokklánc technológia, aminél Szilárd véleménye szerint nincsen rettenetesebb adatbázis.",
+        yt: "https://www.youtube.com/watch?v=6qSw4kspPH8",
+    },
+    {
+        name: "Szabadok legyünk, s ne rabok!",
+        img: "/pics/bitcoin-kebab-230417.png",
+        date: "2023.04.17.",
+        members: ["Kata", "Árpi"],
+        topic: "A 'Bitcoin kontra CBDC avagy “szabadok legyünk, s ne rabok”' című előadásról, központi bankokról, Lightning Network-ről, tanácsok kezdők számára.",
+        yt: "https://www.youtube.com/watch?v=rpD8o6V-j_k",
+    }
+]
+
