fa667e297162700e396c05be00443b57062c0bc6
Lez <lez@github.com>
2025-05-13 14:17:32 +0200

    Added Pizza Day

---
+++ src/views/PizzaView.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <div name="main-inner" class="container">
+      <div class="container-sm py-3 px-3 mx-auto" style="min-height: 400px;">
+        <img src="/pics/pizzaday.jpg" style="max-width: 800px;"><br>
+        <h1>Pizza Nap a Dunakanyarban</h1>
+
+<p>
+    Idén 15 éve történt a világ első Bitcoinos vásárlása egy magyar származású programozó, Hanyecz László által.
+    Ebből az alkalomból a Dunakanyarban több helyen lehet majd Bitcoinnal fizetni. Kiváló minőségű ételeket és helyi termékeket vásárolhattok, továbbá tettrekész bitcoinosokkal találkozhattok.
+</p>
+<p>
+Néhány gyors infó:
+<li>A Bitcoin egy valuta, ami nem inflálódik és tetszőlegesen darabolható, így akár 10 forintot is tarthatsz bitcoinban</li>
+<li>Egy mobil appot kell csak feltelepíteni, és feltölteni bitcoinnal ahhoz, hogy fizetni tudj vele. Nem kell hozzá varázslónak lenni.</li>
+<li>Egy tranzakció néhány forintba vagy fillérbe kerül és pár másodperc alatt lezajlik.</li>
+<li>A Bitcoin már nem csak a kockák szórakozása, hanem az infláció elleni védekezés hatékony eszköze.</li>
+</p>
+
+<p>
+Ha most kezded a bitcoinozást, javasoljuk, hogy a "Blink Bitcoin Wallet"-et (iPhoneon "Blink (Bitcoin Beach Wallet)") telepítsd fel a telefonodra mielőtt elindulsz otthonról. Ez egy egyszerűen használható tárca, ami ráadásul helyi termék, hiszen hazai az egyik fejlesztője.
+Tegyél a pénztárcába valamennyi Bitcoint, hogy pizzára, kajára, piára, házi lekvárra, és tanyasi sajtra is jusson! (útmutató hamarosan) Ebben mi is tudunk segíteni a Miegymásban reggel 10-től délig.
+</p>
+
+<h3>Helyszínek</h3>
+<p>
+A Piknikben változatos kajákat, hamburgert, sütiket, sülteket vagy épp indiai fűszerezéssel készült egytálételeket kaptok. Érdemes elidőzni egy kicsit a mediterrán hangulatú helyen a Duna-parton.
+</p>
+<p>
+Délután a Zsengélőben, Verőce kulturális középpontjában egy kiváló pizza mellett, a diófa árnyékában tapasztalt bitcoinosok fognak a kérdéseitekre válaszolni, praktikus tippeket adni, segíteni wallet témában, és abban, hogy hozzájussatok azokhoz a kevésbé ismert, ám fontos részletekhez, amik az újságcikkekből gyakran kimaradnak.
+</p>
+<p>A harmadik helyszínt később jelentjük be.</p>
+
+<h3>Info tapasztalt bitcoinosoknak</h3>
+<p>
+Bármelyik bitcoin wallet jó, ami támogat lightning-ot. A pultos egy QR kódot fog eléd rakni, ami egy Lightning invoice - ezt a bitcoin lightning walleteddel le kell olvasni, így tudsz fizetni. A Zsengélőben hardveres pénztárcákkal is találkozhatsz, és megnézheted, hogy működnek.
+</p>
+
+<h3>Linkek</h3>
+<p>
+<a href="https://inflacio.eu/mi-az-a-bitcoin-pizza-day/">Több infó a pizza day történetéről</a>
+</p>
+<p>
+<a href="https://www.facebook.com/piknikmanufaktura">Piknik Manufaktúra</a>
+</p>
+<p>
+<a href="https://www.facebook.com/zsengelocafe ">Zsengélő Café</a>
+</p>
+
+<h3>Odajutás</h3>
+(info később)
+
+      </div>
+    </div>
+  </main>
+</template>
+
