d42237bf128765d3a8147d36c8cc31ec00eb7ce3
optout <13562139+optout21@users.noreply.github.com>
2025-06-27 13:14:14 +0200

    Break up Pods into PodsList and Pods, with max count

--- src/components/Pods.vue
+++ src/components/Pods.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { podcasts } from '../data/podcasts'
+import PodsList from './PodsList.vue'
 import { kebabs } from '../data/kebabs'
 import { link_simplified } from '../utils'
 </script>
@@ -15,35 +15,7 @@ import { link_simplified } from '../utils'
     <p>
         Kizárólag Bitcoinnal foglalkozunk, és fő célunk a magyar nyelvű Bitcoin témájú tájékoztatás mennyiségének, minőségének, közérthetőségének és valóságtartalmának intenzív javítása.
     </p>
-    <div name="links-list" class="container-flex py-3 podcast">
-        <div class="row row-cols-2">
-            <div v-for="pod in podcasts" class="card mx-2 my-2" style="width: 20rem;">
-                <div class="card-body">
-                    <h4 class="my-3">
-                        <a :href="pod.yt" target="_blank" class="link">
-                            <img :src="pod.img" :alt="pod.name" :title="pod.name" />
-                        </a>
-                    </h4>
-                    <h5 class="top">
-                        {{ pod.name }}
-                    </h5>
-                    <p class="small my-3 grey">
-                        {{ pod.date }}&nbsp;<span class="dark">|</span>&nbsp;{{ pod.id }}<span v-for="member in pod.members">&nbsp;<span class="dark">|</span>&nbsp;{{ member }}</span>
-                    </p>
-                    <p class="small my-3">
-                        youtube: 
-                        <a :href="pod.yt" target="_blank" class="link">video</a>
-                        &nbsp;|&nbsp;
-                        fountain: 
-                        <a :href="pod.fountain" target="_blank" class="link">audio</a>
-                    </p>
-                    <p class="small my-3">
-                        {{ pod.topic }}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <PodsList :max_count = 0 />
 
     <h2 id="bitcoin-kebab">Bitcoin Kebab podcast</h2>
     <p>
