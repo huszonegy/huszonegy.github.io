4f595bfd9a32ab7cf1d90546e86433076427c501
optout <13562139+optout21@users.noreply.github.com>
2025-12-28 17:46:34 +0100

    Meetup portal page (#295)

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -37,6 +37,9 @@
                     <li class="nav-item">
                         <router-link class="nav-link" to="/tarcak">Tárcák</router-link>
                     </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/meetup">Meetup</router-link>
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
