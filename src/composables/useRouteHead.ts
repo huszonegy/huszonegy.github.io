f4a6199a021a5fcfdae7f9815f7d72ae8d0831e7
Arpad Muranyi <pesz@pesz.hu>
2026-03-14 17:05:18 +0100

    lang: 'hu' fix

--- src/composables/useRouteHead.ts
+++ src/composables/useRouteHead.ts
@@ -5,6 +5,10 @@ import { computed } from 'vue'
 export function useRouteHead() {
   const route = useRoute()
   useHead({
+    htmlAttrs: {
+      lang: 'hu',
+      // ha később light mode is kell: 'data-bs-theme': 'dark' (de most statikusan is maradhat)
+    },
     title: computed(() => (route.meta.title as string) || 'HUSZONEGY - csak bitcoinról, magyarul'),
     meta: computed(() =>
       ((route.meta.metaTags as Array<{ name: string; content: string }>) || []).map((t) => ({
