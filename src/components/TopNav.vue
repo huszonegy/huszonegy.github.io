5789e1f8dade6634e8c57e0754499748307e6bad
Arpad Muranyi <pesz@pesz.hu>
2026-03-27 09:40:00 +0100

    menu update

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -14,9 +14,6 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/forum">Fórum</router-link>
-                    </li>
-                    <li class="nav-item">
                         <router-link class="nav-link" to="/hir">Hír</router-link>
                     </li>
                     <li class="nav-item">
@@ -45,20 +42,6 @@
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" to="/meetup">Meetup</router-link>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Whitepaper
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/files/bitcoin_tanulmany.pdf" target="_blank">
-                                    Bitcoin tanulmány (HU)
-                                </a></li>
-                            <li><a class="dropdown-item" href="/files/bitcoin.pdf" target="_blank">
-                                    Bitcoin whitepaper (eredeti, EN)
-                                </a></li>
-                        </ul>
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" to="/tamogatas">Támogatás</router-link>
